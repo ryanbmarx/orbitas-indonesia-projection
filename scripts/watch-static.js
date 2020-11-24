424f7b6ee47cd1f970ee45942dd354e95b992c54
Ryan Marx <ryanbmarx@gmail.com>
2020-11-24 11:13:34 -0600

    Add static function

---
+++ scripts/watch-static.js
@@ -0,0 +1,18 @@
+const chokidar = require("chokidar");
+const { exec } = require("child_process");
+
+const watchedDirectories = process.argv.slice(2);
+
+/**
+ * Take a list of directories, assumed to be in ./src, and watches them for changes to the contents.
+ * If changes are detected, then it runs the corresponding npn script static:{dir}.
+ */
+
+watchedDirectories.forEach(d => {
+  console.log(`+++ Now watching ${d}`);
+  const watcher = chokidar.watch(`./src/${d}`, { persistent: true });
+  watcher.on("change", path => {
+    console.log(`+++ ${path} has changed. Running npm run static:${d}`);
+    exec(`npm run static:${d}`);
+  });
+});
